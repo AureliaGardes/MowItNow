4b07725bc2d51441f916f8b52b0b54bd2958bfbc
Aurelia Gardes <Aurelia.Gardes@external.totaldirectenergie.com>
2020-07-06 16:01:05 +0200

    original commit

--- src/test/java/MowItNowTest.java
+++ src/test/java/MowItNowTest.java
@@ -1,15 +1,14 @@
 import static fr.xebia.MowItNow.LE_FICHIER_N_A_PAS_ETE_TROUVE;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.doCallRealMethod;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.verify;
 
 import java.io.FileNotFoundException;
-import java.util.Optional;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -23,34 +22,85 @@ public class MowItNowTest {
 
 
     @Mock
+    MowItNow mowItNowMock = new MowItNow("tondeuse.txt");
+
     MowItNow mowItNow = new MowItNow("tondeuse.txt");
 
 
 
     @Test(expected = FileNotFoundException.class)
     public void ouvertureFichierTestKo() throws FileNotFoundException {
-        doCallRealMethod().when(mowItNow).ouvertureFichier(any());
-        mowItNow.ouvertureFichier("inconu");
+        doCallRealMethod().when(mowItNowMock).ouvertureFichier(any());
+        mowItNowMock.ouvertureFichier("inconu");
     }
 
     @Test
     public void ouvertureFichierTestOk() throws FileNotFoundException {
-        doCallRealMethod().when(mowItNow).ouvertureFichier(any());
-        assertNotNull(mowItNow.ouvertureFichier("tondeuse.txt"));
+        doCallRealMethod().when(mowItNowMock).ouvertureFichier(any());
+        assertNotNull(mowItNowMock.ouvertureFichier("tondeuse.txt"));
     }
 
     @Test
     public void initFileKoFileNotFoundTest() throws FileNotFoundException {
-
-        doCallRealMethod().when(mowItNow).ouvertureFichier(any());
-        doThrow(FileNotFoundException.class).when(mowItNow).ouvertureFichier(any());
-
-        assertTrue(mowItNow.initFile("tondeuseTest.txt").isEmpty());
-        verify(mowItNow).handleError(LE_FICHIER_N_A_PAS_ETE_TROUVE, anyString());
+        doCallRealMethod().when(mowItNowMock).initFile(any());
+        doThrow(FileNotFoundException.class).when(mowItNowMock).ouvertureFichier(any());
+        assertTrue(mowItNowMock.initFile("tondeuseTest.txt").isEmpty());
+        verify(mowItNowMock).handleError(LE_FICHIER_N_A_PAS_ETE_TROUVE, null);
     }
 
+    @Test
+    public void TondreOkTest(){
+    assertEquals("5 1 E", mowItNow.run());
+    }
 
+    @Test
+    public void TondreKOFichierVideTest() {
+        mowItNow.setNomFichier("FichierVide.txt");
+        assertEquals("Erreur de lecture du fichier, programme interrompu", mowItNow.run());
+    }
 
+    @Test
+    public void TondreKOFichierInvalideTest() {
+        mowItNow.setNomFichier("MoinsDe3Lignes.txt");
+        assertEquals("Erreur de lecture du fichier, programme interrompu", mowItNow.run());
+    }
 
+    @Test
+    public void TondreKOFichierLignesPairesTest() {
+        mowItNow.setNomFichier("lignesPaires.txt");
+        assertEquals("Erreur de lecture du fichier, programme interrompu", mowItNow.run());
+    }
+
+    @Test
+    public void TondreKOpremiereLigneInvalideTest() {
+        mowItNow.setNomFichier("premiereLigneInvalide.txt");
+        assertEquals("Erreur de configuration de la pelouse, programme interrompu.", mowItNow.run());
+    }
+
+    @Test
+    public void TondreKOTaillePelouseInvalide_1Test() {
+        mowItNow.setNomFichier("TaillePelouseInvalide_1.txt");
+        assertEquals("Erreur de configuration de la pelouse, programme interrompu.", mowItNow.run());
+    }
+
+    @Test
+    public void TondreKOTaillePelouseInvalide_2Test() {
+        mowItNow.setNomFichier("TaillePelouseInvalide_2.txt");
+
+        assertEquals("Erreur de configuration de la pelouse, programme interrompu.", mowItNow.run());
+    }
+
+    @Test
+    public void TondreKOMauvaiseDirectionTest() {
+        mowItNow.setNomFichier("CommandeNonReconnue.txt");
+
+        assertEquals("Erreur intervenue durant le déplacement de la tondeuse, programme interrompu.",  mowItNow.run());
+    }
+
+    @Test
+    public void TondreKOMauvaiseOrientationTondeuseTest() {
+        mowItNow.setNomFichier("MauvaiseOrientationTondeuse.txt");
+        assertEquals("Impossible de déterminer la position de la tondeuse, programme interrompu.", mowItNow.run());
+    }
 
 }
